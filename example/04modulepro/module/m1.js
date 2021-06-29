9df73f63d31fae4c37d04efeb4ba240083ed106a
f117-serect <15159885771@163.com>
2021-06-29 11:50:55 +0800

    example

---
+++ example/04modulepro/module/m1.js
@@ -0,0 +1,8 @@
+let star = '保安小张'
+
+  sing = (title) =>{
+    
+    console.log(title)
+}
+
+export {star,sing}
---
+++ example/04modulepro/module/m3.js
@@ -0,0 +1,10 @@
+export default {
+    username: '保安小张',
+    age: 20,
+    coding =() => {
+        
+        console.log('hello,world')
+    
+    },
+    
+}
