365c0ccd046671f4610178895c37b0e1ffac680a
f117-serect <15159885771@163.com>
2021-07-08 18:02:59 +0800

    借款人信息页面

---
+++ srm-admin/src/views/core/borrow-info/detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+    <h4>借款信息</h4>
+    <table
+      class="table table-striped table-condenseda table-bordered"
+      width="100%"
+    >
+      <tbody>
+        <tr />
+        <tr />
+        <tr />
+        <tr />
+      </tbody>
+    </table>
+
+    <h4>借款人信息</h4>
+
+  </div>
+</template>
